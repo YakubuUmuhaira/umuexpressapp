33475d25f15bc879f3096ed0baa379be2819a50d
YakubuUmuhaira <umuhairayakubu86@gmail.com>
2021-04-07 13:11:44 +0000

    second express commit

---
+++ farm.js
@@ -0,0 +1,16 @@
+
+
+const farmers = [
+    {name:"farmer1", product:"maize", id:"f001"},
+    {name:"farmer2", product:"soya beans", id:"f002"},
+    {name:"farmer3", product:"cow pea", id:"f003"},
+    {name:"farmer4", product:"millet", id:"f004"},
+    {name:"farmer5", product:"yam", id:"f005"},
+    {name:"farmer6", product:"potato", id:"f006"},
+    {name:"farmer7", product:"mango", id:"f007"},
+    {name:"farmer8", product:"pea", id:"f008"},
+    {name:"farmer9", product:"banana", id:"f009"},
+    {name:"farmer10", product:"casava", id:"f0010"}
+]
+
+module.exports = farmers;
--- server.js
+++ server.js
@@ -1,19 +1,82 @@
-const express = require("express")
+const express = require("express");
+const farmers = require("./farm");
 const app = express()
 
 const port = 5000;
 
-app.get("/" ,(req, res)=> {
-    res.send("welcome to UMU's api")
-})
-app.post("/post_request" ,(req, res)=> {
-    res.send("You can post your data here")
-})
-app.delete("/del_request" ,(req, res)=> {
-    res.send("You can DELETE from here")
-})
-app.put("/p_request" ,(req, res)=> {
-    res.send("This is the PUT route")
+// body parser
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+// get all farmers
+
+app.get("/farmapi/v1/farmers" ,(req, res)=> {
+    res.send(farmers)
 })
 
-app.listen(port, ()=> console.log(`the server has started ${port}`));
+// get a single farmer
+app.get("/farmapi/v1/farmers/:id", (req, res)=> {
+    const id = req.params.id;
+    const farmer = farmers.some((f)=> f.id ===id);
+
+    if (farmer) {
+        res.json(farmers.filter((farmer)=> farmer.id === id));
+    } else {
+        res.status(404).json({message: "not found"});
+    }
+});
+ 
+// create a new farmer
+
+app.post("/farmapi/v1/farmers", (req, res)=> {
+    const newf = {
+        name: req.body.name,
+        crop: req.body.crop,
+        id: req.body.id,
+    };
+
+    farmers.push(newf);
+
+    res.json(farmers);
+})
+// delete farmer
+app.delete("/farmapi/v1/farmers/:id" ,(req, res)=> {
+    const id = req.params.id;
+    const farmer = farmers.some((f) => f.id === id);
+
+    if (farmer) {
+        res.json({
+            msg: `Farmer deleted  ${id}`,
+            farmers: farmers.filter((f)=> f.id !==id),
+        })
+    }
+
+    
+});
+// updating a farmer information
+app.put("farmapi/v1/farmers/:id" ,(req, res)=> {
+    const id = req.params.id;
+    const farmer = farmers.some((f)=> f.id === id);
+
+    const newName = req.body.name
+    const newCrop = req.body.crop
+
+    if (farmer) {
+        farmers.forEach((f)=>{
+            if(f.id === id) {
+                (f.name=newName ? newName : f.name),
+                (f.crop=newCrop ? newCrop : f.crop);
+            }
+        });
+        
+        res.json(farmers);
+    } else {
+        res.status(404).json({message: "not found"})
+    }
+   
+})
+
+
+app.listen(port,()=> console.log(`server is operational ${port}`))
